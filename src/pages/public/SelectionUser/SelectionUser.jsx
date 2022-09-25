041e6c6f026fba73fc33253dd0c4c8cd833d60f3
HectorGerardoZV <torrozapata@gmail.com>
2022-09-25 02:36:39 -0700

    SelectionUser page was finished

--- src/pages/public/SelectionUser/SelectionUser.jsx
+++ src/pages/public/SelectionUser/SelectionUser.jsx
@@ -1,8 +1,23 @@
+import IMAGE_STUDENT from "/images/img-estudent-selection.svg";
+import IMAGE_COMPANY from "/images/img-company-selection.svg";
 
+import style from "./SelectionUser.module.css"
 const SelectionUser = () => {
-  return (
-    <div>SelectionUser</div>
-  )
-}
+    return (
+        <div className={style.selectionUser}>
+            <h2><span className={style.importantText}>Registrarse</span> como</h2>
+            <div className={style.selectionUser__options}>
+                <div className={style.selectionUser__option}>
+                    <img src={IMAGE_STUDENT} alt="student-selection" />
+                    <button>Estudiante</button>
+                </div>
+                <div className={style.selectionUser__option}>
+                    <img src={IMAGE_COMPANY} alt="company-selection" />
+                    <button>Empresa</button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default SelectionUser
+export default SelectionUser;
