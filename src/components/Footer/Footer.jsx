14f5e69c27d61f8ab692a7097e1269bc08bd38c2
HectorGerardoZV <torrozapata@gmail.com>
2022-09-24 23:59:28 -0700

    Footer section was finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
+import ICON_ITSON from "/icons/logo.svg";
+import ICON_FACEBOOK from "/icons/icon-facebook.svg";
+import ICON_INSTA from "/icons/icon-instagram.svg";
+import ICON_TWITTER from "/icons/icon-twitter.svg";
 
+import style from "./Footer.module.css";
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <div className={style.footer}>
+            <div className={style.footer__logo}>
+                <img src={ICON_ITSON} alt="icon-itson" />
+            </div>
+            <div className={style.footer__social}>
+                <a href="https://www.facebook.com/itson.mx" target={"_blank"}>
+                <img src={ICON_FACEBOOK} alt="icon-facebook" />
+                </a>
+                <a href="https://www.instagram.com/itsonoficial/" target={"_blank"}>
+                    <img src={ICON_INSTA} alt="icon-instagram" />
+                </a>
+                <a href="https://twitter.com/itson" target={"_blank"}>
+                    <img src={ICON_TWITTER} alt="icon-twitter" />
+                </a>
+            </div>
+        </div>
+    );
+};
 
-export default Footer
+export default Footer;
